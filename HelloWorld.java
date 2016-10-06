6e22a63e564baaf98a9e77591f87ad043c661289
JonnySQ <cra15005@byui.edu>
2016-10-05 19:16:13 -0600

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,16 +1,7 @@
-<<<<<<< HEAD
-
-class Hello{
-	public static void main(String[] args)
-		{
-			System.out.println("Hello World!")
-		}
-}
-=======
 class Hello{
 	public static void main(String[] args)
 		{
 			System.out.println("Hello GitHub World!")
 		}
 }
->>>>>>> 8c6e93b19b7de2f30dc2b8cf714522da4482c9be
+
